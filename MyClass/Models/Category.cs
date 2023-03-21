a86b49ffb36788705e087852f15543297c055cd0
unknown <thelinhboy@gmail.com>
2023-03-21 21:23:33 +0700

    first commit

---
+++ MyClass/Models/Category.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass.Models
+{
+    [Table("Categorys")]
+    public class Category
+    {
+        [Key]
+        public int Id {get; set;}
+        [Required(ErrorMessage = "Tên loại sản phẩm không để trống!")]
+        [StringLength(255)]
+        public string Name { get; set;}
+
+        public string Slug { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public int? Orders { get; set; }
+
+        [Required]
+        public string MetaDesc { get; set; }
+
+        [Required]
+        public string MetaKey { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public int? UpdatedBy { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public int Status { get; set; }
+    }
+}
---
+++ SLT_shopLap/Areas/Admin/ADMINAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SLT_shopLap.Areas.ADMIN
+{
+    public class ADMINAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ADMIN";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ADMIN_default",
+                "ADMIN/{controller}/{action}/{id}",
+                new { Controller="Dashboard", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
